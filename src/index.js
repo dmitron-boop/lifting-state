b1bc2f547db29ee550bcbccb8da0f11f6c2803c0
Dmitry <zero.boop@gmail.com>
2020-09-27 00:57:08 +0300

    '

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import CvI from './CvI';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <CvI />
   </React.StrictMode>,
   document.getElementById('root')
 );
